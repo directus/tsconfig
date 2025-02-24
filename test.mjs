4234ff82d64cfa10857481a76c3d2190a08e2399
Pascal Jufer <pascal-jufer@bluewin.ch>
2025-02-24 14:44:36 +0100

    Fix new lint issues

--- test.mjs
+++ test.mjs
@@ -1,16 +1,14 @@
-/* eslint-disable no-console */
-
 import { readdir } from 'node:fs/promises';
-import { dirname, join, relative } from 'node:path';
+import path from 'node:path';
 import process from 'node:process';
 import { fileURLToPath } from 'node:url';
 import c from 'tinyrainbow';
 import { parseNative } from 'tsconfck';
 import ts from 'typescript';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const configs = await readdir(join(__dirname, 'configs'));
+const configs = await readdir(path.join(__dirname, 'configs'));
 
 const results = await Promise.allSettled(configs.map((config) => parseNative(`configs/${config}/tsconfig.json`)));
 
@@ -18,16 +16,21 @@ for (const [index, result] of results.entries()) {
 	const config = configs[index];
 
 	if (result.status === 'fulfilled') {
-		console.log(c.green('✔'), c.bold(config));
+		log(c.green('✔'), c.bold(config));
 		continue;
 	}
 
 	const message = result.reason?.diagnostic ? formatDiagnostic(result.reason.diagnostic) : String(result.reason);
 
-	console.log(c.red('✖'), c.bold(config));
-	console.log(message.replace(/^/gm, '  > '));
+	log(c.red('✖'), c.bold(config));
+	log(message.replaceAll(/^/gm, '  > '));
 
 	process.exitCode = 1;
+}
+
+function log(...message) {
+	// eslint-disable-next-line no-console
+	console.log(...message);
 }
 
 function formatDiagnostic(diagnostic) {
@@ -36,7 +39,7 @@ function formatDiagnostic(diagnostic) {
 	if (!diagnostic.file)
 		return message;
 
-	const file = relative('.', diagnostic.file.fileName);
+	const file = path.relative('.', diagnostic.file.fileName);
 	const { line, character } = ts.getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start);
 
 	return `${file} (${line + 1},${character + 1}): ${message}`;
